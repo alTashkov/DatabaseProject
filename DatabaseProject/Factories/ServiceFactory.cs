5ede9f775b64663d1e7b4b6c77fa5da5d4ceb2e0
Alexandar Tashkov <alexandar.tashkov@p3-group.com>
2025-12-19 15:25:10 +0200

    Refactor CLI. Add summary to service factory method. Add more data to input files.

--- DatabaseProject/Factories/ServiceFactory.cs
+++ DatabaseProject/Factories/ServiceFactory.cs
@@ -12,6 +12,12 @@ namespace DatabaseProject.Factories
             _scope = scope;
         }
 
+        /// <summary>
+        /// Resolves a service and makes it generic, if it's not one already.
+        /// </summary>
+        /// <param name="entityType">The type of entity being processed.</param>
+        /// <param name="serviceType">The service type being processed.</param>
+        /// <returns>The resolved generic service.</returns>
         public object ResolveProcessor(Type entityType, Type serviceType)
         {
             if (!serviceType.IsGenericTypeDefinition)
--- DatabaseProject/Program.cs
+++ DatabaseProject/Program.cs
@@ -13,7 +13,7 @@ namespace DatabaseProject
             ContainerConfig.Configure(builder);
 
             var container = builder.Build();
-            using (var scope = container.BeginLifetimeScope()) 
+            using (ILifetimeScope scope = container.BeginLifetimeScope()) 
             {
                 SocialMediaContext context = scope.Resolve<SocialMediaContext>();
                 context.Database.Migrate();
@@ -21,13 +21,13 @@ namespace DatabaseProject
                 Console.WriteLine("Social media DBMS: ");
 
                 bool isEndOperation = false;
+                var opeartionManager = scope.Resolve<OperationManager>();
                 while (!isEndOperation)
                 {
                     Console.WriteLine("\nPlease choose an operation \n1. Insert\n2. Read\n3. Update\n4. Delete\n5. Exit");
                     Console.WriteLine("\nOperation:");
                     if (int.TryParse(Console.ReadLine(), out int operationType))
                     {
-                        var opeartionManager = scope.Resolve<OperationManager>();
                         if (operationType != 5)
                         {
                             switch (operationType)
@@ -35,66 +35,24 @@ namespace DatabaseProject
                                 case 1:
                                     Console.WriteLine("\nOPERATION - Insert");
                                     Console.WriteLine("Please enter path to file to read from: ");
-
-                                    string? inputFilePath = Console.ReadLine();
-                                    if (!(string.IsNullOrEmpty(inputFilePath)))
-                                    {
-                                        opeartionManager.Insert(inputFilePath);
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Invalid input file path! Please try again.");
-                                        break;
-                                    }
-                                break;
-
+                                    opeartionManager.Insert(Console.ReadLine() ?? "");
+                                    break;
                                 case 2:
                                     Console.WriteLine("\nOPERATION - Read");
                                     Console.WriteLine("Please enter path to file to read to: ");
-
-                                    string? outputFilePath = Console.ReadLine();
-                                    if (!(string.IsNullOrEmpty(outputFilePath)))
-                                    {
-                                        opeartionManager.ReadWithFilter(outputFilePath);
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Invalid output file path! Please try again.");
-                                        break;
-                                    }
-                                break;
-
+                                    opeartionManager.ReadWithFilter(Console.ReadLine() ?? "");
+                                    break;
                                 case 3:
                                     Console.WriteLine("\nOPERATION - Update");
                                     Console.WriteLine("Enter entity type: ");
-
-                                    string? updateEntityType = Console.ReadLine();
-                                    if (!(string.IsNullOrEmpty(updateEntityType)))
-                                    {
-                                        opeartionManager.Update(updateEntityType);
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Invalid entity type! Please try again.");
-                                        break;
-                                    }
-                                break;
+                                    opeartionManager.Update(Console.ReadLine() ?? "");
+                                    break;
 
                                 case 4:
                                     Console.WriteLine("\nOPERATION - Delete");
                                     Console.WriteLine("Enter entity type: ");
-
-                                    string? deleteEntityType = Console.ReadLine();
-                                    if (!(string.IsNullOrEmpty(deleteEntityType)))
-                                    {
-                                        opeartionManager.Delete(deleteEntityType);
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Invalid entity type! Please try again.");
-                                        break;
-                                    }
-                                break;
+                                    opeartionManager.Delete(Console.ReadLine() ?? "");
+                                    break;
                             }
                         }
                         else
